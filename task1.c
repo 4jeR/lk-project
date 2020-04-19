05d71f7acd5237c2659f0345b17d54cfe6f00857
4jeR <bdlugosz98@gmail.com>
2020-04-19 17:37:40 +0200

    update

--- task1.c
+++ task1.c
@@ -1,21 +1,42 @@
 /*
-Bartłomiej Długosz - Jądro systemu operacyjnego na przykładzie Linux.
+Bartłomiej Długosz - Jądro systemu na przykładzie LINUX
 
 ZADANIE 1. - Własne polecenie find (System Calls)
-PRZYGOTOWANIE:
-a) Uruchom skrypt: ./run_dlugosz_bartlomiej.sh clone
-b) Stwórz przykładowe dane: ./run_dlugosz_bartlomiej.sh run
+CEL ZADANIA:
+Wykorzystanie w praktyce wywołań systemowych - 
+komunikacją pomiędzy jądrem systemu a programem.
 
-Napisz program, który znajdzie i wypisze na standardowe wyjście
+### PRZYGOTOWANIE:
+a) Uruchom skrypt: 
+
+./run_dlugosz_bartlomiej.sh clone
+
+b) Stwórz przykładowe dane:
+
+./run_dlugosz_bartlomiej.sh run
+
+=== TREŚĆ ZADANIA ===
+
+Napisz program w języku C, który znajdzie i wypisze na standardowe wyjście
 wszystkie pliki oraz katalogi, które w swojej nazwie zawierają 
 dany ciąg znaków (zadany jako argument wywołania programu).
 Wyniki powinny być wypisane z widocznymi wcięciami, aby zaznaczyć
 ścieżkę, w której się znajdują.
 
 
-./plik_wykonywalny ciag_znakow
+./my_find ciag_znakow
 
-WSKAZÓWKA: pomocne będzie przestudiowanie manuala: opendir, readdir, string.
+WSKAZÓWKA: pomocne będzie przestudiowanie manuala m.in. opendir, readdir, string.
+ 
+Na koniec
+Aby wyczyścić to, co zostało ściągnięte przez skrypt 'clone' należy użyć flagi 'clean'.
+
+./run_dlugosz_bartlomiej.sh clean
+
+UWAGA:
+USUNIE TO RÓWNIEŻ TEN PLIK !!!
+
+Link do wideoprezentacji: https://www.youtube.com/watch?v=9qwW-VgKIz0
 */
 
 
