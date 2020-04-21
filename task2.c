0c849ab618e6ff870cc86190263c6cf4c2e5602c
Bartłomiej Długosz <38592844+4jeR@users.noreply.github.com>
2020-04-21 10:51:12 +0200

    Update task2.c

--- task2.c
+++ task2.c
@@ -9,14 +9,16 @@ dodając/zmieniając nowe elementy w nim + wykonanie części drugiej.
 @@@
 
 (1) tym razem wynik z konsoli zapisz w buforze (zakładamy, że rozmiar nie przekroczy 1000 znaków).  
-(2) następnie z poziomu programu stwórz plik o nazwie „result.txt”, który będzie miał następujące prawa dostępu: grupa oraz inni będą mieli prawo do odczytu, natomiast użytkownik ma posiadać prawa do odczytu oraz zapisu.  
-Uwaga: zabronione jest użycie funkcji:
+(2) następnie z poziomu programu stwórz plik o nazwie „result.txt”, który będzie miał następujące prawa 
+    dostępu: grupa oraz inni będą mieli prawo do odczytu, natomiast użytkownik ma posiadać prawa do odczytu oraz zapisu.  
+    Uwaga: zabronione jest użycie funkcji:
 
-int open(const char *pathname, int flags, mode_t mode);  
-int open(const char *pathname, int flags);
+    int open(const char *pathname, int flags, mode_t mode);  
+    int open(const char *pathname, int flags);
 
-Użyj odpowiedniego system calla, aby pozyskać informację na temat uprawnień do jego dostępu, które wypiszesz na konsolę.  
-(3) teraz, przy użyciu system calla zmień prawa dostepu do pliku „result.txt” na 707. Ponownie wypisz jego prawa dostępu na konsolę.  
+    Użyj odpowiedniego system calla, aby pozyskać informację na temat uprawnień do jego dostępu, które wypiszesz na konsolę.  
+(3) teraz, przy użyciu system calla zmień prawa dostepu do pliku „result.txt” na 707. 
+    Ponownie wypisz jego prawa dostępu na konsolę.  
 (4) następnie przywróć plik „result.txt” do praw dostępu z punktu 2), używając drugiego sposobu na zmianę a następnie wypisz tę informację na konsolę.  
 (5) na koniec należy zamknąć plik, który otworzyliśmy oraz zwolnić wszelką zaalokowaną pamięć. 
 */
