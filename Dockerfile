35ff1cb0ea8c0b0057655ab07f38aa73ea6d0253
4jeR <bdlugosz98@gmail.com>
2020-04-21 11:50:57 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,11 @@ FROM ubuntu:20.04
 
 RUN apt-get update -y
 RUN apt-get install -y gcc nano 
-RUN echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" | sudo \
-   tee /etc/apt/sources.list.d/vs-code.list
-
-RUN curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-RUN mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-RUN sudo apt-get update
-RUN sudo apt-get install code
 
 
-COPY README.md /home/root/
-COPY skrypt.pdf /home/root
-
+COPY find_here /home/root/find_here/
+COPY task1.c /home/root
+COPY task2.c /home/root
+COPY run_dlugosz_bartlomiej.sh /home/root
 
 WORKDIR /home/root
--- run_dlugosz_bartlomiej.sh
+++ run_dlugosz_bartlomiej.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
-if [ $1 = "clone" ]; then
+if [ $1 = "clone" ]
+then
     git clone https://github.com/4jeR/lk-project.git lk-project
     rm -rf lk-project/.git
-elif [ $1 = "run" ]; then
+elif [ $1 = "run" ]
+then
     if [[ -d "lk-project" ]]
-    echo "[run]"
     then
         cd lk-project
     fi
-    
-elif [ $1 = "clean" ]; then
+
+    echo "      "
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    echo "@ Budowanie środowiska dockerowego. @"
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    echo "      "
+
+    docker build -t dlugoszbartlomiej:1.0 .
+
+    echo "      "
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    echo "@ Budowanie środowiska dockerowego. OK @"
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    echo "      "
+
+    echo "      "
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    echo "@       Włączanie środowiska.       @"
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    echo "      "
+
+    docker run -it dlugoszbartlomiej:1.0
+
+    echo "      "
+
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    echo "@       Włączanie środowiska.  OK   @"
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    echo "      "
+elif [ $1 = "clean" ]
+then
+    if [[ ! -d "lk-project" ]]
+    then
+        cd ..
+    fi    
     rm -rf lk-project
-    rm -rf find_here
 else
     echo "Podaj opcję: clone / run / clean"
 fi
